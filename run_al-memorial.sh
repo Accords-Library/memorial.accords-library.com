aabb228dfd77e46b3f49b5f7e2a838614883dc8f
DrMint <29893320+DrMint@users.noreply.github.com>
2025-11-03 18:39:37 +0100

    Fixed params not going through

--- run_al-memorial.sh
+++ run_al-memorial.sh
@@ -1,4 +1,4 @@
 rm -r dist
 npm ci
 npm run build
-npm run preview --port 45343 --host --allowed-hosts=accords-library.com
+npm run preview -- --port 45343 --host --allowed-hosts=accords-library.com
